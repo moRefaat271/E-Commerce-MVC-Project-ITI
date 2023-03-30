edd11c006e55dd8f175d67402c3b6a539096a829
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-03-30 07:31:33 +0200

    last update by fathy
    fixing layouts for all pages, added admin dashboard, added some controllers and views

--- ECommerce_MVC_Project_ITI/ECommerce/Controllers/payPalController.cs
+++ ECommerce_MVC_Project_ITI/ECommerce/Controllers/payPalController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
 {
+    [Authorize]
     public class PayPalController : Controller
     {
         public IActionResult Index()
